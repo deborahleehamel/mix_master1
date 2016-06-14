de2daddc654b0b37dbdf26ef29b705be18fdbabe
Deborah Hamel <deborahleehamel@gmail.com>
2016-06-14 16:37:14 -0600

    Implemented validations at model level.

--- spec/features/user_creates_an_artist_spec.rb
+++ spec/features/user_creates_an_artist_spec.rb
@@ -27,4 +27,28 @@ RSpec.feature "User submits a new artist" do
     expect(page).to have_content artist_name
     expect(page).to have_css("img[src=\"#{artist_image_path}\"]")
   end
+
+
+  context "the submitted data is invalid" do
+    scenario "they see error message" do
+      # As a User
+      artist_image_path = "http://cps-static.rovicorp.com/3/JPG_400/MI0003/146/MI0003146038.jpg"
+
+      # When I visit the artist index
+      visit artists_path
+
+      # and I click on "New Artist"
+      click_on "New Artist"
+
+      # and I don't fill in name
+      # and I fill in image_path
+      fill_in "artist_image_path", with: artist_image_path
+
+      # and I click on "Create Artist"
+      click_on "Create Artist"
+
+      # then I should see "Name connot be blank" on the page
+      expect(page).to have_content "Name can't be blank"
+    end
+  end
 end
